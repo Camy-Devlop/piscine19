64c579c931dfa5b597ede320f350d601a44c91bc
Camy-Devlop <adbaibi.ismail@gmail.com>
2021-12-06 14:20:18 +0100

    test alias

---
+++ c00/ft_is_negative1.c
@@ -0,0 +1,16 @@
+//
+//  Created by adbaibi
+#include <unistd.h>
+#include "ft_is_negative.h"
+void ft_is_negative(int n)
+{
+		char *tab=NULL;
+		tab="NP";
+		if(n<0)
+		{
+			write(1,&(tab[0]),1);
+		}else
+		{
+			write(1,&(tab[1]),1);
+		}
+}
